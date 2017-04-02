35a2a370e9d1eb2979fbaff215bdc0e164a829a7
MrDarkman <mrdark6996@gmail.com>
2017-04-02 16:36:54 +0700

    Comment Facebook meta tag

--- themes/vwalldectheme/functions.php
+++ themes/vwalldectheme/functions.php
@@ -302,36 +302,36 @@ function pietergoosen_comments( $comment, $args, $depth ) {
 
 
 
-//Adding the Open Graph in the Language Attributes
-function add_opengraph_doctype( $output ) {
-    return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
-}
-add_filter('language_attributes', 'add_opengraph_doctype');
+////Adding the Open Graph in the Language Attributes
+//function add_opengraph_doctype( $output ) {
+//    return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+//}
+//add_filter('language_attributes', 'add_opengraph_doctype');
 
 //Lets add Open Graph Meta Info
 
-function insert_fb_in_head() {
-    global $post;
-    if ( !is_singular()) //if it is not a post or a page
-        return;
-    echo '<meta property="fb:admins" content="YOUR USER ID"/>';
-    echo '<meta property="og:title" content="' . get_the_title() . '"/>';
-    echo '<meta property="og:type" content="article"/>';
-    echo '<meta property="og:description" content="'. get_the_content().'" />';
-    echo '<meta property="og:url" content="' . get_permalink() . '"/>';
-    echo '<meta property="og:site_name" content="www.vwalldec.com"/>';
-    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
-        $default_image="http://example.com/image.jpg"; //replace this with a default image on your server or an image in your media library
-        echo '<meta property="og:image" content="' . $default_image . '"/>';
-    }
-    else{
-        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
-        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
-    }
-    echo "
-";
-}
-add_action( 'wp_head', 'insert_fb_in_head', 5 );
+//function insert_fb_in_head() {
+//    global $post;
+//    if ( !is_singular()) //if it is not a post or a page
+//        return;
+//    echo '<meta property="fb:admins" content="YOUR USER ID"/>';
+//    echo '<meta property="og:title" content="' . get_the_title() . '"/>';
+//    echo '<meta property="og:type" content="article"/>';
+//    echo '<meta property="og:description" content="'. get_the_content().'" />';
+//    echo '<meta property="og:url" content="' . get_permalink() . '"/>';
+//    echo '<meta property="og:site_name" content="www.vwalldec.com"/>';
+//    if(!has_post_thumbnail( $post->ID )) { //the post does not have featured image, use a default image
+//        $default_image="http://example.com/image.jpg"; //replace this with a default image on your server or an image in your media library
+//        echo '<meta property="og:image" content="' . $default_image . '"/>';
+//    }
+//    else{
+//        $thumbnail_src = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'medium' );
+//        echo '<meta property="og:image" content="' . esc_attr( $thumbnail_src[0] ) . '"/>';
+//    }
+//    echo "
+//";
+//}
+//add_action( 'wp_head', 'insert_fb_in_head', 5 );
 
 
 if(function_exists('add_social_button_in_content')) { echo add_social_button_in_content(); }
